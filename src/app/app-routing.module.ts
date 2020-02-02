91682012ddf37372cf46d3651c9646fc5bb1d8eb
Jackseed <vinkho.c@gmail.com>
2020-02-02 22:14:46 +0100

    first step login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TileComponent } from './tile/tile.component';
+import { AuthGuard } from './auth/auth.guard';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: UserProfileComponent },
+  { path: 'play', component: TileComponent,  canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
