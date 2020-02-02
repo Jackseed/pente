bbf9b77a3103f541a7c276d07c9b484a6d4ece4b
Jackseed <vinkho.c@gmail.com>
2020-02-02 11:25:37 +0100

    add capture scores

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -5,6 +5,10 @@ import {MatButtonModule} from '@angular/material/button';
 export interface Tile {
   color: string;
 }
+export interface Player {
+  color: string;
+  score: number;
+}
 
 @Component({
   selector: 'app-tile',
@@ -13,6 +17,14 @@ export interface Tile {
 })
 export class TileComponent implements OnInit {
   tile: Tile[] = [];
+  whitePlayer: Player = {
+    color: 'white',
+    score: 0,
+  };
+    blackPlayer: Player = {
+    color: 'black',
+    score: 0,
+  };
   activePlayer = 'black';
   passivePlayer = 'white';
   neighbour1: number;
@@ -36,17 +48,17 @@ export class TileComponent implements OnInit {
       for (y = -1; y <= 1; y++) {
         if (
           // check if tile[i] is not in the first 3 lines and in the last 3
-          ((i + 57 * y < 361) && (i + 57 * y >= 0)) 
+          ((i + 57 * y < 361) && (i + 57 * y >= 0))
           // check if tile[i] is not in the first 3 columns and in the last 3
           || (((i % 19 > -3 * x) && (i % 19 < 19 + -3 * x) ))
           ) {
-            console.log("neighbour 1:", this.neighbour1, "neighbour 2:", this.neighbour2, "neighbour 3:", this.neighbour3);
+            console.log('neighbour 1:', this.neighbour1, 'neighbour 2:', this.neighbour2, 'neighbour 3:', this.neighbour3);
             // 3 next spots on a specific direction
             this.neighbour1 = i + (x + 19 * y);
             this.neighbour2 = i + 2 * (x + 19 * y);
             this.neighbour3 = i + 3 * (x + 19 * y);
             if (
-              // avoid undifined tiles 
+              // avoid undifined tiles
               (this.neighbour1 >= 0 && this.neighbour1 < 361)
               && (this.neighbour2 >= 0 && this.neighbour2 < 361)
               && (this.neighbour3 >= 0 && this.neighbour3 < 361)
@@ -57,6 +69,11 @@ export class TileComponent implements OnInit {
                 // capture of the tiles
                 this.tile[this.neighbour1].color = 'grey';
                 this.tile[this.neighbour2].color = 'grey';
+                if (this.activePlayer === 'black') {
+                  this.blackPlayer.score = this.blackPlayer.score + 2;
+                } else {
+                  this.whitePlayer.score = this.whitePlayer.score + 2;
+                }
             }
         } else {
           return;
