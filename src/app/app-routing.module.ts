2f68574d6c6d00641fb98f95b5b781818cd3eda5
Jackseed <vinkho.c@gmail.com>
2020-02-06 09:19:19 +0100

    deploy does not work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TileComponent } from './tile/tile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'game',
+    component: TileComponent,
+    data: { title: 'Pente Game' }
+  },
+  { path: '',
+  redirectTo: '/game',
+  pathMatch: 'full'
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
